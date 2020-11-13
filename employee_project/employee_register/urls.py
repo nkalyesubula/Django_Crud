f92cf5033cd19e7e2774fa35fced1bcac296cf3f
nkalyesubula <noahkalyesubula@gmail.com>
2020-11-13 14:24:41 +0300

    chore(integrate react and django api)

--- employee_project/employee_register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.employee_form, name="employee_insert"), #get and post
-    path('<int:id>/', views.employee_form, name='employee_update'),
-    path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
-    path('list/', views.employee_list, name="employee_list"),
-]
